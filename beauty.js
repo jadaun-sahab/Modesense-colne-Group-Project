64d8cb370df2acf37b2fe20d0ac924413dc3b51d
Swati <swatitani01@gmail.com>
2022-12-16 18:09:18 +0530

    Day-4_BeautyPage

--- beauty.js
+++ beauty.js
@@ -1,28 +1,88 @@
-let http = new XMLHttpRequest();
-http.open('get','API/beauty.json',true);
-http.send();
-http.onload = function(){
-    if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText);
-        let output = "";
-        for(let item of products){
-            output +=  `
-            <div class="product">
-                <img src="${item.avatar}" alt="${item.avatar}">  
-                <p class="title">${item.title}</p>
-                <p class="price">
-                    <span>&#8377;</span>
-                    <span>${item.price}</span>  
-                </p> 
-                <p class="qty">Qty: ${item.qty}</p>
-                <p class="rating">Rating: ${item.rating}</p>
-                <p class="id">${item.id}</p>
-                <button class="button" style="font-size:17px">Add to <i class="fa fa-shopping-cart"></i></button>
-            </div>
-            `;
-        }
-        document.querySelector(".products").innerHTML = output;
-    }
+let bag = [];
+fetch("API/beauty.json")
+.then((res)=>res.json())
+.then((data)=>{
+    bag=data;
+    displayData(bag);
+})
+
+function displayData(bag){ 
+    let xyz = document.querySelector(".products");
+    xyz.innerHTML = "";
+    
+    let newArray = bag.map((item) => {
+        return `<div class="product">
+        <img src="${item.avatar}" alt="${item.avatar}">  
+        <p class="title">${item.title}</p>
+        <p class="price">
+            <span>&#8377;</span>
+            <span>${item.price}</span>  
+        </p> 
+        <p class="qty">Qty: ${item.qty}</p>
+        <p class="rating">Rating: ${item.rating}</p>
+        <button class="button" style="font-size:17px">Add to cart<i class="fa fa-shopping-cart"></i></button>
+    </div>`;
+    })
+    
+    xyz.innerHTML = newArray.join("");
 }
-console.log("abc")
+
+document.querySelector(".products").innerHTML = bag;
+
+document.querySelector("#reset").addEventListener("click",function(event){
+        displayData(bag);    
+});
+
+document.querySelector("#brand").addEventListener("click",function(event){
+    if(document.querySelector("#brand").value == "Brands"){
+        displayData(bag);
+    }     
+});
+
+document.querySelector("#brand").addEventListener("change",function(event){
+    let selected = document.querySelector("#brand").value;
+    let filteredData = bag.filter(function(element){
+        return element.brand == selected;
+    });
+        displayData(filteredData);
+});
+
+document.querySelector("#categories").addEventListener("change",function(event){
+    let selected = document.querySelector("#categories").value;
+    let filteredData = bag.filter(function(element){
+        return element.categories == selected;
+    });
+        displayData(filteredData);
+});
+
+document.querySelector("#rating").addEventListener("change",function(event){
+    let selected = document.querySelector("#rating").value;
+    let filteredData = bag.filter(function(element){
+        if(element.rating)
+        return element.rating == selected;
+    });
+        displayData(filteredData);
+});
+
+function handleSort(){
+    let selected = document.querySelector("#price").value;
+    if(selected=="HTL"){
+        bag.sort((a,b)=>b.price-a.price);
+    }
+    if(selected=="LTH"){
+        bag.sort((a,b)=>a.price-b.price);
+    }
+    displayData(bag);
+}
+    
+function search(){
+    let query = document.querySelector("#search").value;
+    console.log(query);
+    let newData = bag.filter(function(element){
+        return element.title.toLowerCase().includes(query.toLowerCase());
+    });
+    console.log(newData);
+    displayData(newData);
+}
+
 
