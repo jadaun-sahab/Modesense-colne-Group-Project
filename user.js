6839b1a03833fe488808eda43469e96b89ffff36
vikas jadaun <deadpoolji125@gmail.com>
2023-06-25 14:30:45 +0530

    update

--- user.js
+++ user.js
@@ -1,6 +1,18 @@
-const toggleButton = document.getElementsByClassName('bx bx-menu')[0]
-    const navbarLinks = document.getElementsByClassName('sidebar')[0]
+// const toggleButton = document.getElementsByClassName('bx bx-menu')[0]
+//     const navbarLinks = document.getElementsByClassName('sidebar')[0]
    
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    }) 
+//     toggleButton.addEventListener('click', () => {
+//         navbarLinks.classList.toggle('active')
+//     }) 
+
+var MenuItem = document.getElementById("sidebar");
+
+MenuItem.style.display = "none";
+
+function menutoggle() {
+  if (MenuItem.style.display == "none")
+   MenuItem.style.display = "block";
+  else {
+    MenuItem.style.display = "none";
+  }
+}
