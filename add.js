99a5186c85903cc2567623f26a705f4ce5c80c0b
vikas jadaun <deadpoolji125@gmail.com>
2023-06-26 14:17:24 +0530

    update

--- add.js
+++ add.js
@@ -119,3 +119,14 @@ UpdateButton.addEventListener("click",()=>{
     })
 })
 
+var MenuItem = document.getElementById("sidebar");
+
+MenuItem.style.display = "none";
+
+function menutoggle() {
+  if (MenuItem.style.display == "none")
+   MenuItem.style.display = "block";
+  else {
+    MenuItem.style.display = "none";
+  }
+}
--- manage.js
+++ manage.js
@@ -62,3 +62,18 @@ function Products(Data){
     }) 
 }
 
+
+
+
+
+var MenuItem = document.getElementById("sidebar");
+
+MenuItem.style.display = "none";
+
+function menutoggle() {
+  if (MenuItem.style.display == "none")
+   MenuItem.style.display = "block";
+  else {
+    MenuItem.style.display = "none";
+  }
+}
