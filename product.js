d6d4fb3cef2e39069ff45f384eba1ad0b6f874e0
vikas jadaun <deadpoolji125@gmail.com>
2023-06-26 13:55:44 +0530

    update

--- product.js
+++ product.js
@@ -41,3 +41,16 @@ function Products(Data){
     })
 }
 
+
+
+var MenuItem = document.getElementById("sidebar");
+
+MenuItem.style.display = "none";
+
+function menutoggle() {
+  if (MenuItem.style.display == "none")
+   MenuItem.style.display = "block";
+  else {
+    MenuItem.style.display = "none";
+  }
+}
