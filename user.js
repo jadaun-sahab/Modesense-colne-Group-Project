4430d9fa1c719b43d7aae190ac9e998f0ab5a744
vikas jadaun <deadpoolji125@gmail.com>
2023-06-26 14:19:54 +0530

    update

--- user.js
+++ user.js
@@ -1,14 +1,8 @@
-// const toggleButton = document.getElementsByClassName('bx bx-menu')[0]
-//     const navbarLinks = document.getElementsByClassName('sidebar')[0]
-   
-//     toggleButton.addEventListener('click', () => {
-//         navbarLinks.classList.toggle('active')
-//     }) 
+
+// js for sidebar toogle button  
 
 var MenuItem = document.getElementById("sidebar");
-
 MenuItem.style.display = "none";
-
 function menutoggle() {
   if (MenuItem.style.display == "none")
    MenuItem.style.display = "block";
@@ -16,3 +10,5 @@ function menutoggle() {
     MenuItem.style.display = "none";
   }
 }
+
+// js for sidebar toogle button  
